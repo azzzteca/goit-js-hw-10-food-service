4982964eda0e78a518ba562744bd83b768db5139
Andrii Shovkun <andrii.shovkun@gmail.com>
2021-06-29 16:29:09 +0300

    change theme done

--- src/index.js
+++ src/index.js
@@ -2,25 +2,27 @@ import menuItems from './menu.json';
 import menuTemplate from './templates/menu-template.hbs';
 import './styles.css';
 
-const menu = document.querySelector('.js-menu');
+const refs = {
+  menu: document.querySelector('.js-menu'),
+  switcher: document.getElementById('theme-switch-toggle'),
+  body: document.querySelector('body'),
+};
 
-menu.innerHTML = menuTemplate(menuItems);
+refs.menu.innerHTML = menuTemplate(menuItems);
 
+refs.body.classList.add('light-theme');
 
-// 1. Создать разметку для добавления элементов меню
-// - получить доступ к разделу, куда будем рендерить разметку
-// - создать шаблон одного элемента
-// - модифицировать шаблон одного элемента под массив
-// - 
-// 
-// 
-// 2. Написать код включения - выключения светлой темы на темную 
+refs.switcher.addEventListener('change', () => {
+  refs.body.classList.toggle('dark-theme');
+});
+//
+// 2. Написать код включения - выключения светлой темы на темную
 // - получить доступ к кнопке
 // - повесить слушателя на кнопку
 // - по событию менять стиль темы - меять класс?
-// 
-// 
+//
+//
 
-// 
-// 
-// 
+//
+//
+//
